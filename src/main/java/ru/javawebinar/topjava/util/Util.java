4033b85fb4aeb6c8c97f605bc3d6f07b88389b50
gkislin <github13>
2020-02-17 23:59:40 +0300

    make time filter half open

--- src/main/java/ru/javawebinar/topjava/util/Util.java
+++ src/main/java/ru/javawebinar/topjava/util/Util.java
@@ -7,6 +7,6 @@ public class Util {
     }
 
     public static <T extends Comparable<? super T>> boolean isBetweenInclusive(T value, @Nullable T start, @Nullable T end) {
-        return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) <= 0);
+        return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
     }
 }
